b4de20b07ce3ee962820ea6fc5bdaab88493c66d
Rodrigofds <rodrigoldiscool@gmail.com>
2021-11-09 11:53:19 -0300

    Criado função para marcar o jogador X ao tabuleiro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 var player = null;
+var selectedPlayer = document.getElementById('jogador-selecionado');
+
+changePlayer('X');
 
 function chooseSquare(id){
-    console.log(id);
+    var square = document.getElementById(id);
+
+    square.innerHTML = player;
+    square.style.color = '#000';
+}
+
+function changePlayer(value){
+    player = value;
+    selectedPlayer.innerHTML = player;
 }
